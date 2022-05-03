d5ddd748e603e3e55ba9c2c1ac9b4eaa2b5c8be6
alicftc <aciftci5@pratt.edu>
2022-05-03 15:21:38 -0400

    -

---
+++ includes/search.php
@@ -0,0 +1,10 @@
+
+<form class="searchform"  method="get" action='results.php'>
+
+    
+    <label class="searchlabel" for="search">Search for a user</label>
+    <input class="searchinput" type="text" name="search">
+    <input class="searchbutton" type="submit" name="submit" value="Search">
+
+</form>
+  
